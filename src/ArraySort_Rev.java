989abb977389963297dc56007a475e8be81db07e
Anubhav Koul <anubhavcgc.mca11@gmail.com>
2022-10-18 01:45:09 +0530

    Added the code for sorting an Array by comparing the previous element

--- src/ArraySort_Rev.java
+++ src/ArraySort_Rev.java
@@ -1,8 +1,22 @@
-
+import java.util.Random;
+/* Sorting an Array by comparing the previous elements. */
 public class ArraySort_Rev {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Random rand = new Random();
+		int[] arr = { 2, 3, 4, 5, 7, 6, 4, 1, 7, 3, 5, 2 };
+
+		System.out.println("---------");
+		for (int i = 1; i < arr.length; i++) {
+			int currentValue = arr[i];
+			int j = i - 1;
+			while (j >= 0 && arr[j] > currentValue) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = currentValue;
+		}
+		for (int element : arr)
+			System.out.println(element);
 
 	}
 
